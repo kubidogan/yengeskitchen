00916786aa04f358989fe9cb99a6a726cc9eedf0
Kubi Dogan <kubi_g@hotmail.co.uk>
2023-05-18 23:25:48 +0100

    made changes to header and styling of header

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <Header/>
+        <Header title="yenges kitchen" />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
 const Header = () => {
   return (
-    <div>Yenges Kitchen Test</div>
+    <header>
+      <h1>Yenges Kitchen</h1>
+      <nav>
+          <select>
+            <option value="item1">Item 1</option>
+            <option value="item2">Item 2</option>
+            <option value="item3">Item 3</option>
+            <option value="item4">Item 4</option>
+          </select>
+        </nav>
+    </header>
   )
 }
 
