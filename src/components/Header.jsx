335db5c6820a60489fb266dfd312991083cb2abe
Kubi Dogan <kubi_g@hotmail.co.uk>
2023-05-22 23:44:05 +0100

    added logo to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
+import logo from '../assets/logo.png'
 const Header = () => {
   return (
     <header>
       <h1>Yenges Kitchen</h1>
       <nav>
-          <select>
-            <option value="item1">Item 1</option>
-            <option value="item2">Item 2</option>
-            <option value="item3">Item 3</option>
-            <option value="item4">Item 4</option>
-          </select>
-        </nav>
+        <img src={logo} alt="Yenges Kitchen logo"/>
+      </nav>
     </header>
   )
 }
